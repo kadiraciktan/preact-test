b7059d2bdac4873511d2f8ffbba1368b0df613fb
Kadir AÇIKTAN <k.aciktan@ulogi.com>
2023-05-11 17:18:54 +0300

    feat : added toast example

--- src/components/my-button.component.jsx
+++ src/components/my-button.component.jsx
@@ -5,6 +5,8 @@ export const styles = {
   dark: "bg-black text-white rounded p-2 m-2",
 };
 let currentTheme = signal(styles.light);
+let toasts = signal([]);
+
 export function MyButtonComponent() {
   const changeTheme = () => {
     if (currentTheme.value === styles.light) {
@@ -12,11 +14,38 @@ export function MyButtonComponent() {
     } else {
       currentTheme.value = styles.light;
     }
+
+    const random = Math.random();
+
+    toasts.value.push(random);
+
+    setTimeout(() => {
+      toasts.value = toasts.value.filter((toast) => toast !== random);
+    }, 3000);
   };
 
   return (
-    <button className={currentTheme.value} onClick={changeTheme}>
-      {currentTheme.value === styles.light ? "Light" : "Dark"}
-    </button>
+    <div>
+      <button className={currentTheme.value} onClick={changeTheme}>
+        {currentTheme.value === styles.light ? "Light" : "Dark"}
+      </button>
+
+      {toasts.value.map((message) => {
+        return (
+          <div
+            className="toast"
+            onClick={() => {
+              toasts.value = toasts.value.filter((toast) => toast !== message);
+            }}
+          >
+            <div className="alert alert-info cursor-pointer">
+              <div>
+                <span>{message}</span>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 }
