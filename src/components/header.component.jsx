f56d872ab78ea232d4eef9668a825da49d3d9081
Kadir AÇIKTAN <k.aciktan@ulogi.com>
2023-05-11 16:37:34 +0300

    feat : created preact with tailwind app

---
+++ src/components/header.component.jsx
@@ -0,0 +1,3 @@
+export function HeaderComponent() {
+  return <h1 className="text-3xl font-bold underline">Header Component!</h1>;
+}
---
+++ src/pages/index-page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HeaderComponent } from "../components";
+
+export function IndexPage() {
+  return (
+    <div>
+      <HeaderComponent />
+    </div>
+  );
+}
